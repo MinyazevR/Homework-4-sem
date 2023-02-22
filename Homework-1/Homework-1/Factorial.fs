7cf3874d04ea4863c0af415ab56967b14691a6ac
MinyazevR <eeuriset8@gmail.com>
2023-02-23 00:16:28 +0300

    fixed

--- Homework-1/Homework-1/Factorial.fs
+++ Homework-1/Homework-1/Factorial.fs
@@ -4,11 +4,11 @@
 /// Factorial function
 /// </summary>
 /// <param name="n">Number</param>
-/// <returns>n! if n >= 0 else 0</returns>
+/// <returns>n! if n >= 0 else None</returns>
 let factorial n =
-    if n < 0 then 0 else 
+    if n < 0 then None else 
     let rec fac n = 
         match n with
         | 0 | 1 -> 1
         | _ -> n * fac (n - 1)
-    fac n
+    Some (fac n)
--- Homework-1/Homework-1/Fibonacci.fs
+++ Homework-1/Homework-1/Fibonacci.fs
@@ -4,11 +4,11 @@
 /// Function for calculating the nth Fibonacci number
 /// </summary>
 /// <param name="n">Number</param>
-/// <returns> Nth fibonacci number if n >= 0 else -1 </returns>
+/// <returns> Nth fibonacci number if n >= 0 else None </returns>
 let fibonacci n =
-    if n < 0 then -1 else
+    if n < 0 then None else
     let rec fib n iter (a, b) =
         if iter < n then fib n (iter + 1) (b, a + b)
         else b
-    if n = 0 then 0 else fib n 1 (0, 1)
+    if n = 0 then Some 0 else Some (fib n 1 (0, 1))
 
