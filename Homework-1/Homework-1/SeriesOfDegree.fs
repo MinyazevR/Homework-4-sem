afe7619140bc22e9b7a50aa3298158211d92f87b
MinyazevR <eeuriset8@gmail.com>
2023-02-23 12:08:58 +0300

    fixed SeriesOfDegree.fs

--- Homework-1/Homework-1/SeriesOfDegree.fs
+++ Homework-1/Homework-1/SeriesOfDegree.fs
@@ -6,9 +6,4 @@
 /// <param name="n">Indicator of the first degree</param>
 /// <param name="m">Indicator of the last degree</param>
 /// <returns>Returns a list of powers of 2 from n to n + m</returns>
-let seriesOfDegree n m =
-    let rec multiplyLastElemOn2 ls count iter state =
-        if iter < count then multiplyLastElemOn2 (List.append ls [state * 2.0]) count (iter + 1) (state * 2.0)
-        else ls
-    let answer = [2.0 ** n]
-    multiplyLastElemOn2 answer m 0 answer.Head
+let seriesOfDegree n m = [ for i in n .. n + m -> 2. ** i ]
