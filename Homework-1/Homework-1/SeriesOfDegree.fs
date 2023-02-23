bbb606780620ebe6aedf2d4158e835d2efedaffb
MinyazevR <eeuriset8@gmail.com>
2023-02-23 13:33:47 +0300

    The number of exponentiations has decreased

--- Homework-1/Homework-1/SeriesOfDegree.fs
+++ Homework-1/Homework-1/SeriesOfDegree.fs
@@ -6,4 +6,6 @@
 /// <param name="n">Indicator of the first degree</param>
 /// <param name="m">Indicator of the last degree</param>
 /// <returns>Returns a list of powers of 2 from n to n + m</returns>
-let seriesOfDegree n m = [ for i in n .. n + m -> 2. ** i ]
+let seriesOfDegree n m =
+    let upper_bound = 2.0 ** (n + m)
+    2.0  ** n |> List.unfold (fun state -> if state > upper_bound then None else Some (state, state * 2.0))
