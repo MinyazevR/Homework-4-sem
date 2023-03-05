8f8898a14d792c4fceccbb880c6f68ca9e389181
MinyazevR <eeuriset8@gmail.com>
2023-03-06 02:16:33 +0300

    fixed Factorial.fs

--- Homework-1/Homework-1/Factorial.fs
+++ Homework-1/Homework-1/Factorial.fs
@@ -9,10 +9,10 @@ let factorial n =
     if n < 0 then
         None
     else
-        let rec fac n =
+        let rec fac n acc =
             match n with
             | 0
-            | 1 -> 1
-            | _ -> n * fac (n - 1)
+            | 1 -> acc
+            | _ -> fac (n - 1) acc * n
 
-        Some(fac n)
+        Some(fac n 1)
