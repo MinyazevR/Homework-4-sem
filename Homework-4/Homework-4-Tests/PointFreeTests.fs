5699aa0725bcc1b7791741d5ae029c006f93820c
MinyazevR <eeuriset8@gmail.com>
2023-03-31 23:52:18 +0300

    add PointFree solution and tests

---
+++ Homework-4/Homework-4-Tests/PointFreeTests.fs
@@ -0,0 +1,18 @@
+﻿module PointFreeTests
+
+open FsCheck
+open PointFree
+open FsUnit
+
+let firstCheck x l =
+    first_step x l = second_step x l
+    
+let secondCheck x l =
+    second_step x l = third_step x l
+    
+let thirdCheck x l =
+    third_step x l = fourth_step x l
+    
+Check.QuickThrowOnFailure firstCheck
+Check.QuickThrowOnFailure secondCheck
+Check.QuickThrowOnFailure thirdCheck
---
+++ Homework-4/Homework-4/PointFree.fs
@@ -0,0 +1,7 @@
+﻿module PointFree
+
+let first_step x l = List.map (fun y -> x * y) l
+let second_step x = List.map (fun y -> (*) x y)
+let third_step x = List.map ((*) x)
+let fourth_step = (*) >> List.map 
+
