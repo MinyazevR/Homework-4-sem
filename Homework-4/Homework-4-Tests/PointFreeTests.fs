ff047277de04c2b30c228a7b87d8ba3427f10247
MinyazevR <eeuriset8@gmail.com>
2023-04-01 01:41:17 +0300

    fixed tests

--- Homework-4/Homework-4-Tests/PointFreeTests.fs
+++ Homework-4/Homework-4-Tests/PointFreeTests.fs
@@ -1,18 +1,20 @@
 ﻿module PointFreeTests
 
 open FsCheck
+open NUnit.Framework
 open PointFree
-open FsUnit
 
-let firstCheck x l =
-    first_step x l = second_step x l
-    
-let secondCheck x l =
-    second_step x l = third_step x l
-    
-let thirdCheck x l =
-    third_step x l = fourth_step x l
-    
-Check.QuickThrowOnFailure firstCheck
-Check.QuickThrowOnFailure secondCheck
-Check.QuickThrowOnFailure thirdCheck
+let firstCheck x l = first_step x l = second_step x l
+
+let secondCheck x l = second_step x l = third_step x l
+
+let thirdCheck x l = third_step x l = fourth_step x l
+
+[<Test>]
+let first_test () = Check.QuickThrowOnFailure firstCheck
+
+[<Test>]
+let second_test () = Check.QuickThrowOnFailure secondCheck
+
+[<Test>]
+let third_test () = Check.QuickThrowOnFailure thirdCheck
--- Homework-4/Homework-4/PointFree.fs
+++ Homework-4/Homework-4/PointFree.fs
@@ -3,5 +3,4 @@
 let first_step x l = List.map (fun y -> x * y) l
 let second_step x = List.map (fun y -> (*) x y)
 let third_step x = List.map ((*) x)
-let fourth_step = (*) >> List.map 
-
+let fourth_step = List.map << (*)
