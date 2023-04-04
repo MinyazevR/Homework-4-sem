de7c04c1abdb7425d923f40ead394a545884c530
MinyazevR <eeuriset8@gmail.com>
2023-04-05 00:07:13 +0300

    Added a solution for the problem of the correctness of the bracket sequence and tests for it

---
+++ Homework-4/Homework-4-Tests/ParenthesisTests.fs
@@ -0,0 +1,31 @@
+﻿module ParenthesisTests
+
+open NUnit.Framework
+open FsUnit
+open Homework_4.Parenthesis
+
+let testCaseData =
+    [
+        checkCorrectOrderBrackets "", Success;
+        checkCorrectOrderBrackets "()", Success;
+        checkCorrectOrderBrackets "{}", Success;
+        checkCorrectOrderBrackets "[]", Success;
+        checkCorrectOrderBrackets "[({})]", Success;
+        checkCorrectOrderBrackets "([()])", Success;
+        checkCorrectOrderBrackets "({}[]([]))", Success;
+        checkCorrectOrderBrackets "([](){[]})", Success;
+        checkCorrectOrderBrackets "[", Error "incorrect number of parenthesis";
+        checkCorrectOrderBrackets "{", Error "incorrect number of parenthesis";
+        checkCorrectOrderBrackets "(", Error "incorrect number of parenthesis";
+        checkCorrectOrderBrackets ")", Error "incorrect number of parenthesis";
+        checkCorrectOrderBrackets "}", Error "incorrect number of parenthesis";
+        checkCorrectOrderBrackets "]", Error "incorrect number of parenthesis";
+        checkCorrectOrderBrackets "((())}", Error "incorrect bracket";
+        checkCorrectOrderBrackets "(}", Error "incorrect bracket";
+        checkCorrectOrderBrackets "(})", Error "incorrect bracket";
+    ] |> List.map (fun (x, y) -> TestCaseData(x, y))
+        
+
+[<TestCaseSource("testCaseData")>]
+let Test (expected: Result, actual: Result) =
+     actual |> should equal expected
