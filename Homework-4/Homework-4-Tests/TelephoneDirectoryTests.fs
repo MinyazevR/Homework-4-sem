e7cdc0f390819949d5235dea52373e0441513a21
MinyazevR <eeuriset8@gmail.com>
2023-04-09 14:04:42 +0300

    Added tests and comments for TelephoneDirectory

---
+++ Homework-4/Homework-4-Tests/TelephoneDirectoryTests.fs
@@ -0,0 +1,102 @@
+﻿module Homework_4_Tests.TelephoneDirectoryTests
+
+open Homework_4
+open NUnit.Framework
+open FsUnit
+open Homework_4.Parenthesis
+open TelephoneDirectory
+
+[<Test>]
+let AddRecordsTests () =
+    let directory = TelephoneDirectory()
+    let second_directory = directory.Add "Махмур" "12345678"
+
+    second_directory.Entries
+    |> should equal (Set.ofSeq (seq { ("Махмур", "12345678") }))
+
+    let third_directory = second_directory.Add "Сергей" "809543683"
+
+    third_directory.Entries
+    |> should
+        equal
+        (Set.ofSeq (
+            seq {
+                ("Махмур", "12345678")
+                ("Сергей", "809543683")
+            }
+        ))
+
+[<Test>]
+let AddIdenticalRecordsTest () =
+    let directory = TelephoneDirectory()
+    let second_directory = directory.Add "Махмур" "12345678"
+
+    second_directory.Entries
+    |> should equal (Set.ofSeq (seq { ("Махмур", "12345678") }))
+
+    let third_directory = second_directory.Add "Махмур" "12345678"
+
+    third_directory.Entries
+    |> should equal (Set.ofSeq (seq { ("Махмур", "12345678") }))
+
+[<Test>]
+let SuccessFindPhoneByName () =
+    let directory = TelephoneDirectory()
+    let second_directory = directory.Add "Махмур" "12345678"
+
+    second_directory.FindPhoneByName "Махмур"
+    |> should equal (Success(Set.add "12345678" Set.empty))
+
+    let third_directory = second_directory.Add "Махмур" "87654321"
+
+    third_directory.FindPhoneByName "Махмур"
+    |> should
+        equal
+        (Success(
+            Set.ofSeq (
+                seq {
+                    "12345678"
+                    "87654321"
+                }
+            )
+        ))
+
+
+[<Test>]
+let ErrorFindPhoneByName () =
+    let directory = TelephoneDirectory()
+    let second_directory = directory.Add "Махмур" "12345678"
+
+    second_directory.FindPhoneByName "Махма"
+    |> should equal (Error(Set.add "Name not found" Set.empty))
+
+[<Test>]
+let SuccessFindNameByPhone () =
+    let directory = TelephoneDirectory()
+    let second_directory = directory.Add "Махмур" "12345678"
+
+    second_directory.FindNameByPhone "12345678"
+    |> should equal (Success(Set.add "Махмур" Set.empty))
+
+    let third_directory = second_directory.Add "Брат махмура" "12345678"
+
+    third_directory.FindNameByPhone "12345678"
+    |> should
+        equal
+        (Success(
+            Set.ofSeq (
+                seq {
+                    "Махмур"
+                    "Брат махмура"
+                }
+            )
+        ))
+
+
+[<Test>]
+let ErrorFindNameByPhone () =
+    let directory = TelephoneDirectory()
+    let second_directory = directory.Add "Махмур" "12345678"
+
+    second_directory.FindNameByPhone "12345679"
+    |> should equal (Error(Set.add "Phone not found" Set.empty))
--- Homework-4/Homework-4/TelephoneDirectory.fs
+++ Homework-4/Homework-4/TelephoneDirectory.fs
@@ -1,68 +1,88 @@
 ﻿module Homework_4.TelephoneDirectory
 
 type Result =
-    | Success of seq<string>
-    | Error of seq<string>
+    | Success of Set<string>
+    | Error of Set<string>
 
 /// <summary>
 /// Telephone Directory
 /// </summary>
-type TelephoneDirectory(entries: seq<string * string>) =
+type TelephoneDirectory(entries: Set<string * string>) =
     let listOfEntries = entries
-    new() = TelephoneDirectory Seq.empty
+    new() = TelephoneDirectory Set.empty
 
-    member this.Entries
-        with get() = listOfEntries
+    member this.Entries = listOfEntries
 
+    /// <summary>
+    /// Method for adding an entry to the phone directory
+    /// </summary>
+    /// <param name="name"> Name</param>
+    /// <param name="number"> Number</param>
+    /// <returns>If there was no entry, this is a new phone directory with an added entry, otherwise it will return the original phone directory</returns>
     member this.Add name number =
-        TelephoneDirectory(Seq.append listOfEntries (Seq.singleton (name, number)))
+        if Set.contains (name, number) listOfEntries then
+            this
+        else
+            TelephoneDirectory(Set.add (name, number) listOfEntries)
 
 
+    /// <summary>
+    /// Method for finding a phone by number
+    /// </summary>
+    /// <param name="name"> Name</param>
+    /// <returns> Result type from set of phones</returns>
     member this.FindPhoneByName name =
-        let ans =
-            seq {
-                for x, _ in listOfEntries do
-                    if x = name then
-                        x
-            }
+        let ans = Set.filter (fun (x, _) -> x = name) listOfEntries |> Set.map snd
 
-        if ans |> Seq.isEmpty then
-            Error [ "Name not found" ]
+        if ans |> Set.isEmpty then
+            Error(Set.add "Name not found" Set.empty)
         else
             Success ans
 
 
+    /// <summary>
+    /// Method for finding a number by phone
+    /// </summary>
+    /// <param name="phone"> Phone</param>
+    /// <returns> Result type from set of names</returns>
     member this.FindNameByPhone phone =
-        let ans =
-            seq {
-                for _, y in listOfEntries do
-                    if y = phone then
-                        y
-            }
+        let ans = Set.filter (fun (_, y) -> y = phone) listOfEntries |> Set.map fst
 
-        if ans |> Seq.isEmpty then
-            Error [ "Phone not found" ]
+        if ans |> Set.isEmpty then
+            Error(Set.add "Phone not found" Set.empty)
         else
             Success ans
 
+    /// <summary>
+    /// Function for output of records to the console
+    /// </summary>
     member this.OutputEntries =
         for x, y in listOfEntries do
             printf $"Name: {x}, Number: {y}\n"
 
+    /// <summary>
+    /// Function for saving data to a file
+    /// </summary>
+    /// <param name="path"> Path to file</param>
     member this.SaveDataToFile path =
         use writer = new System.IO.StreamWriter(path, false)
 
         for x, y in listOfEntries do
-            writer.WriteLine $"Name: {x}, Number: {y}"
+            writer.WriteLine $"{x} {y}"
 
+    /// <summary>
+    /// Function for reading data from a file
+    /// </summary>
+    /// <param name="path"> Path to file</param>
     member this.ReadDataFromFile(path: string) =
         TelephoneDirectory(
-            seq {
-                use reader = new System.IO.StreamReader(path, false)
+            Set.ofSeq (
+                seq {
+                    use reader = new System.IO.StreamReader(path, false)
 
-                while not reader.EndOfStream do
-                    let x = reader.ReadLine().Split(" ")
-                    yield (x[0], x[1])
-            }
+                    while not reader.EndOfStream do
+                        let x = reader.ReadLine().Split(" ")
+                        yield (x[0], x[1])
+                }
+            )
         )
-        
