fe74a1d21d6b7d8db4d459f01b35cd9310c8506d
MinyazevR <eeuriset8@gmail.com>
2023-04-13 00:18:12 +0300

    Added local network solution and tests

---
+++ Homework-5/Homework-5/LocalNet.fs
@@ -0,0 +1,44 @@
+﻿open System.Collections.Generic
+
+type Computer =
+    { id: int
+      probabilityOfInfection: float }
+
+
+type LocalNet(computers: Map<Computer, seq<Computer>>) =
+    let computers = computers
+    member this.Size = computers.Count
+    member this.GetNeighbors computer = computers[computer]
+
+type Virus(localNet: LocalNet, initiallyInfectedComputers: seq<Computer>) =
+    let localNet = localNet
+    //let initiallyInfectedComputers = initiallyInfectedComputers
+    let mutable infectedComputers = HashSet(initiallyInfectedComputers)
+    let mutable computersMayBeInfected = []
+
+    let addComputersMayBeInfected () =
+        let items =
+            [ for x in infectedComputers do
+                  for t in (localNet.GetNeighbors x) -> t ]
+            |> List.distinct
+            |> List.filter (fun x -> not (infectedComputers.Contains x))
+            |> List.filter (fun x -> x.probabilityOfInfection > 0)
+
+        computersMayBeInfected <- items
+
+    member this.InfectTheComputers() =
+        if localNet.Size = infectedComputers.Count then
+            infectedComputers
+        else
+            addComputersMayBeInfected ()
+
+            if computersMayBeInfected.IsEmpty then
+                infectedComputers
+            else
+                infectedComputers.UnionWith(
+                    List.filter
+                        (fun x -> System.Random().NextDouble() < x.probabilityOfInfection)
+                        computersMayBeInfected
+                )
+
+                infectedComputers
---
+++ Homework-5/LocalNetTests/LocalNetTests.fs
@@ -0,0 +1,53 @@
+module LocalNetTests
+
+open System.Collections.Generic
+open NUnit.Framework
+open FsUnit
+open LocalNet
+
+[<Test>]
+let Test1 () =
+    let firstComputer = { id = 1; probabilityOfInfection = 1.0 }
+    let secondComputer = { id = 1; probabilityOfInfection = 1.0 }
+    let thirdComputer = { id = 1; probabilityOfInfection = 1.0 }
+
+    let adjacencyMap =
+        Map
+            [ (firstComputer,
+               seq {
+                   secondComputer
+                   thirdComputer
+               })
+              (secondComputer, seq { firstComputer })
+              (thirdComputer, seq { firstComputer }) ]
+
+    let localNet = LocalNet(adjacencyMap)
+    let virus = Virus(localNet, seq { firstComputer })
+    let result = virus.InfectTheComputers()
+    let hs = HashSet()
+    hs.Add firstComputer
+    hs.Add secondComputer
+    hs.Add thirdComputer
+    result |> should equal hs
+
+[<Test>]
+let Test2 () =
+    let firstComputer = { id = 1; probabilityOfInfection = 1.0 }
+    let secondComputer = { id = 1; probabilityOfInfection = 0.0 }
+    let thirdComputer = { id = 1; probabilityOfInfection = 0.0 }
+
+    let adjacencyMap =
+        Map
+            [ (firstComputer,
+               seq {
+                   secondComputer
+                   thirdComputer
+               })
+              (secondComputer, seq { firstComputer })
+              (thirdComputer, seq { firstComputer }) ]
+
+    let localNet = LocalNet(adjacencyMap)
+    let virus = Virus(localNet, Seq.empty)
+    let result = virus.InfectTheComputers()
+    let hs = HashSet()
+    result |> should equal hs
