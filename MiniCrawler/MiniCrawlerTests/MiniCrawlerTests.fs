eb51cc851a535d48cd6d205e2ef70d0a379ccb72
MinyazevR <eeuriset8@gmail.com>
2023-05-05 00:07:18 +0300

    formatted

---
+++ MiniCrawler/MiniCrawlerTests/MiniCrawlerTests.fs
@@ -0,0 +1,25 @@
+module MiniCrawlerTests
+
+open NUnit.Framework
+open MiniCrawler
+open FsUnit
+
+[<Test>]
+let firstMiniCrawlerTest () =
+    let answer = work "https://github.com/PrettyBoyCosmo/HTTP-List"
+
+    answer
+    |> Seq.contains (57641, "http://www.socialstudieshelp.com/")
+    |> should equal true
+
+    answer
+    |> Seq.contains (12283, "http://targetedattacks.trendmicro.com/")
+    |> should equal true
+
+    answer |> Seq.contains (200480, "http://blog.adw.org") |> should equal true
+    answer |> Seq.contains (118102, "http://www.hexun.com/") |> should equal true
+
+[<Test>]
+let secondMiniCrawlerTest () =
+    let answer = work "https://www.anekdot.ru/"
+    answer |> Seq.isEmpty |> should equal true
